0f22ba77318781e755212da8429abd6c5b3d2359
Barroada <felipi_gatao@hotmail.com>
2016-06-04 10:37:36 -0300

    Etiquetar v1.0
    + Adicionado Reload antes de etiquetar

--- Etiquetar.js
+++ Etiquetar.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
-   setTimeout(function(){
+   if(sessionStorage.getItem("Att") == 1){
+      sessionStorage.setItem("Att",0);
      $(".selectAll").trigger("click");
      $(".btn[value='Etiqueta']").trigger("click");
+   }else{
+      sessionStorage.setItem("Att",0);
+   }
+   setTimeout(function(){
+      sessionStorage.setItem("Att",1);
+      location.reload();
    },1800000);
 });
